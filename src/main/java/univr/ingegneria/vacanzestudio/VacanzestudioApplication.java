27f3bcf87a12376989338f10b583928e00d18b7d
Marco Signorini <marco.signorini_03@studenti.univr.it>
2021-08-21 18:58:20 +0200

    Initial commit

---
+++ src/main/java/univr/ingegneria/vacanzestudio/VacanzestudioApplication.java
@@ -0,0 +1,13 @@
+package univr.ingegneria.vacanzestudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanzestudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacanzestudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/univr/ingegneria/vacanzestudio/VacanzestudioApplicationTests.java
@@ -0,0 +1,13 @@
+package univr.ingegneria.vacanzestudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanzestudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
