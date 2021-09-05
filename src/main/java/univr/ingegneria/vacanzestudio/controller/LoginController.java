25bac4a868738f8a84d182ad269712069fbdf68b
signocob2 <marco.signorini_03@studenti.univr.it>
2021-09-05 18:40:22 +0200

    Login ritorna intero studente anziché boolean

--- src/main/java/univr/ingegneria/vacanzestudio/controller/LoginController.java
+++ src/main/java/univr/ingegneria/vacanzestudio/controller/LoginController.java
@@ -1,21 +1,37 @@
 package univr.ingegneria.vacanzestudio.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import univr.ingegneria.vacanzestudio.dto.StudenteDto;
+import univr.ingegneria.vacanzestudio.model.Studente;
 import univr.ingegneria.vacanzestudio.service.LoginService;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/login")
 class LoginController {
     @Resource
-    LoginService loginServico;
+    LoginService loginService;
+
+    @Resource
+    ModelMapper modelMapper;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public Boolean getAllAllergia(@RequestParam String email, @RequestParam String password) {
-        return loginServico.isAuthenticatedByEmailAndPassword(email, password);
+    public StudenteDto isAuthenticatedByEmailAndPassword(@RequestParam String email, @RequestParam String password) {
+        Studente studente = loginService.isAuthenticatedByEmailAndPassword(email, password);
+        if (Objects.nonNull(studente)) {
+            return convertToStudenteDto(studente);
+        } else {
+            return null;
+        }
+    }
+
+    private StudenteDto convertToStudenteDto(Studente studente) {
+        return modelMapper.map(studente, StudenteDto.class);
     }
 }
--- src/main/java/univr/ingegneria/vacanzestudio/service/LoginService.java
+++ src/main/java/univr/ingegneria/vacanzestudio/service/LoginService.java
@@ -1,20 +1,17 @@
 package univr.ingegneria.vacanzestudio.service;
 
 import org.springframework.stereotype.Service;
-import univr.ingegneria.vacanzestudio.exception.AllergiaNotFoundException;
-import univr.ingegneria.vacanzestudio.model.Allergia;
-import univr.ingegneria.vacanzestudio.repository.AllergiaRepository;
+import univr.ingegneria.vacanzestudio.model.Studente;
 import univr.ingegneria.vacanzestudio.repository.StudenteRepository;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Service
 public class LoginService {
     @Resource
     private StudenteRepository studenteRepository;
 
-    public boolean isAuthenticatedByEmailAndPassword(String email, String password) {
-        return studenteRepository.findStudenteByEmailAndPassword(email, password) != null;
+    public Studente isAuthenticatedByEmailAndPassword(String email, String password) {
+        return studenteRepository.findStudenteByEmailAndPassword(email, password);
     }
 }
